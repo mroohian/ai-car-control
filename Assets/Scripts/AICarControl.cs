1c62e70ab9cad15411ae7824340c438453c79b91
Reza Roohian <m.roohian87@gmail.com>
2017-08-19 22:22:22 +0200

    Client now supports the new sensors.

--- Assets/Scripts/AICarControl.cs
+++ Assets/Scripts/AICarControl.cs
@@ -9,17 +9,30 @@ public class AICarControl : MonoBehaviour {
     public SensorController SensorLeft;
     public SensorController SensorFront;
     public SensorController SensorRight;
+    public float SteeringSensor;
+    public float SpeedSensor;
+    public float AccelSensor;
+    public float BrakeSensor;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		_carController = GetComponent<CarController>();
 	}
 
     AI_Input GetSensorValues() {
+        SteeringSensor = (_carController.CurrentSteerAngle + 1f) / 2f;
+        SpeedSensor = _carController.CurrentSpeed / _carController.MaxSpeed;
+        AccelSensor = _carController.AccelInput;
+        BrakeSensor = _carController.BrakeInput;
+
         return new AI_Input {
             sensor1 = SensorLeft.Value,
             sensor2 = SensorFront.Value,
-            sensor3 = SensorRight.Value
+            sensor3 = SensorRight.Value,
+            sensor4 = SteeringSensor,
+            sensor5 = SpeedSensor,
+            sensor6 = AccelSensor,
+            sensor7 = BrakeSensor
         };
     }
    
--- Assets/Scripts/InfoPanelUpdator.cs
+++ Assets/Scripts/InfoPanelUpdator.cs
@@ -8,17 +8,27 @@ public class InfoPanelUpdator : MonoBehaviour {
     public Text Sensors1Text;
     public Text Sensors2Text;
     public Text Sensors3Text;
+    public Text Sensors4Text;
+    public Text Sensors5Text;
+    public Text Sensors6Text;
+    public Text Sensors7Text;
+    public Text Sensors8Text;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         _startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		TimeText.text = string.Format("Time: {0:F1}s", Time.time - _startTime);
-        Sensors1Text.text = string.Format("Sensor1: {0:F2}", Car.SensorLeft.Value);
-        Sensors2Text.text = string.Format("Sensor2: {0:F2}", Car.SensorFront.Value);
-        Sensors3Text.text = string.Format("Sensor3: {0:F2}", Car.SensorRight.Value);
-	}
+        Sensors1Text.text = string.Format("S1 (L): {0:F2}", Car.SensorLeft.Value);
+        Sensors2Text.text = string.Format("S2 (F): {0:F2}", Car.SensorFront.Value);
+        Sensors3Text.text = string.Format("S3 (R): {0:F2}", Car.SensorRight.Value);
+        Sensors4Text.text = string.Format("S4 (STE): {0:F2}", Car.SteeringSensor);
+        Sensors5Text.text = string.Format("S5 (SPE): {0:F2}", Car.SpeedSensor);
+        Sensors6Text.text = string.Format("S6 (ACC): {0:F2}", Car.AccelSensor);
+        Sensors7Text.text = string.Format("S7 (BRK): {0:F2}", Car.BrakeSensor);
+        Sensors8Text.text = "";
+    }
 }
